6943ea4b56cd7d19784d1c832485a3b6d934872b
Eduardo Perez Hernandez <57243899+Parzival25@users.noreply.github.com>
2022-05-31 18:44:17 -0500

    migraciones completas

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,13 +16,11 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('las_name');
             $table->string('email')->unique();
             $table->string('image')->nullable();            
             $table->enum('rol', ['Employee', 'Manager','Customer'])->default('Customer');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
