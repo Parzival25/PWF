50e190c7efa6f4d6dc0b6c9d3b0975c18863ed3d
Pamela Durante Cruz <98970012+PamDu01@users.noreply.github.com>
2022-05-31 01:21:39 -0500

    Creacion de Home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <link href="css/home.css" rel="stylesheet">
+
+
+        <title>Home</title>
+
+<html>
+    <head>
+
+    <div class="relative flex items-top justify-center min-h-screen bg-blue-100 dark:bg-blue-900 sm:items-center py-4 sm:pt-0">
+            
+            <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+               
+                    <a href="" class="btn btn-sm btn-success">|  Home  |</a>
+                
+                    <a href="" class="btn btn-sm btn-success">|  Inicia sesión  |</a>
+
+                        <a href="" class="btn btn-sm btn-success">Registrar  |</a>
+                    
+                    <a href="" class="btn btn-sm btn-success">Inicia como invitado  |</a>
+                
+            </div>
+        </div>
+
+    <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+                <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
+                    <img src="/fotos/Even-TEC.jpg" alt="" width="1520" height="680">
+                </div> 
+    </div>
+    </head>
+    
+</html>
