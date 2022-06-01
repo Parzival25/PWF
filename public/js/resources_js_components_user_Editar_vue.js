cc94e63e54f97457372a15ebe9ab5dbc8f7228b0
ItzelGpe <L18270283@tuxtla.tecnm.mx>
2022-05-31 23:29:51 -0500

    Estructura y rutas para el CRUD con SPA

---
+++ public/js/resources_js_components_user_Editar_vue.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["resources_js_components_user_Editar_vue"],{
+
+/***/ "./resources/js/components/user/Editar.vue":
+/*!*************************************************!*\
+  !*** ./resources/js/components/user/Editar.vue ***!
+  \*************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var C_laragon_www_PWF_node_modules_vue_loader_dist_exportHelper_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./node_modules/vue-loader/dist/exportHelper.js */ \"./node_modules/vue-loader/dist/exportHelper.js\");\nconst script = {}\n\n;\nconst __exports__ = /*#__PURE__*/(0,C_laragon_www_PWF_node_modules_vue_loader_dist_exportHelper_js__WEBPACK_IMPORTED_MODULE_0__[\"default\"])(script, [['__file',\"resources/js/components/user/Editar.vue\"]])\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__exports__);//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9yZXNvdXJjZXMvanMvY29tcG9uZW50cy91c2VyL0VkaXRhci52dWUuanMiLCJtYXBwaW5ncyI6Ijs7Ozs7QUFBQTs7QUFFQSxDQUFvRztBQUNwRyxpQ0FBaUMsMEdBQWU7O0FBRWhELGlFQUFlIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vLy4vcmVzb3VyY2VzL2pzL2NvbXBvbmVudHMvdXNlci9FZGl0YXIudnVlPzc3MzQiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc3Qgc2NyaXB0ID0ge31cblxuaW1wb3J0IGV4cG9ydENvbXBvbmVudCBmcm9tIFwiQzpcXFxcbGFyYWdvblxcXFx3d3dcXFxcUFdGXFxcXG5vZGVfbW9kdWxlc1xcXFx2dWUtbG9hZGVyXFxcXGRpc3RcXFxcZXhwb3J0SGVscGVyLmpzXCJcbmNvbnN0IF9fZXhwb3J0c19fID0gLyojX19QVVJFX18qL2V4cG9ydENvbXBvbmVudChzY3JpcHQsIFtbJ19fZmlsZScsXCJyZXNvdXJjZXMvanMvY29tcG9uZW50cy91c2VyL0VkaXRhci52dWVcIl1dKVxuXG5leHBvcnQgZGVmYXVsdCBfX2V4cG9ydHNfXyJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./resources/js/components/user/Editar.vue\n");
+
+/***/ })
+
+}]);
---
+++ resources/js/routers.js
@@ -0,0 +1,23 @@
+
+//Componetes para CRUD usuarios
+const Mostar = ()=> import('./components/user/Mostrar.vue');
+const Crear = ()=> import('./components/user/Crear.vue');
+const Editar = ()=> import('./components/user/Editar.vue');
+
+export const routes = [
+    {
+        name:'mostrarUsers',
+        path:'/users',
+        component:Mostar
+    },
+    {
+        name:'crearUsers',
+        path:'/crear',
+        component:Crear
+    },
+    {
+        name:'EditarUsers',
+        path:'/editar/:id',
+        component:Editar
+    },
+];
