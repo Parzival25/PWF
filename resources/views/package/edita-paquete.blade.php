0dd838d868b4da4ca97b6b7ee93cad2692c68012
Selene Rosales <selenerosales0103@gmail.com>
2022-06-02 11:04:40 -0500

    editar paquete

--- resources/views/package/edita-paquete.blade.php
+++ resources/views/package/edita-paquete.blade.php
@@ -12,26 +12,30 @@
 <br>
 <h2>Editar paquete</h2>
 
-  <form>
+  <form action="" method="POST">
+  @csrf
+  @method('put')
       <br>
       <div class="form-control">
         <label>Nombre</label>
-        <input type="text" class="form-control" id="id_name" placeholder="Escriba el nombre del paquete">
+        <input type="text" name='name' class="form-control" id="id_name" placeholder="Escriba el nombre del paquete" value="{{$package->name}}">
         <br>
         <label>Descripción</label>
       <!--  <input type="text" class="form-control" id="id_description" placeholder="Escriba una breve descripción"> -->
-        <textarea class="form-control" id="id_description" rows="3"></textarea>
+        <textarea class="form-control" name='descripcion' id="id_description" rows="3" value="{{$package->descripcion}}"></textarea>
         <br>
         <label>Imagen</label>
-        <input type="file" class="form-control-file" id="id_precio">
+        <input type="file" name='imagen' class="form-control-file" id="id_precio">
         <br>
         <label>Precio</label>
-        <input type="number" class="form-control" id="id_precio" placeholder="Escriba el precio del paquete">
+        <input type="number" name='price' class="form-control" id="id_precio" placeholder="Escriba el precio del paquete" value="{{$package->price}}">
         <br>
         <label>Estado</label>
-        <select class="form-control" id="id_estado">
+        <select class="form-control" name='status' id="id_estado">
+          @foreach()
           <option>Activo</option>
           <option>Inactivo</option>
+          @endforeach
         </select>
         <br>
       </div>
